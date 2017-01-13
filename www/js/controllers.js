5766684ba484135f26ea3c0b122936f540684ece
Camila Silva <camila.silva57@fatec.sp.gov.br>
2017-01-12 22:11:07 -0200

    troca de alert por popup

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,7 @@
 angular.module('app.controllers', [])
   
-.controller('porNoArCtrl', ['$scope', '$state', 'TreinamentosService', 
-function ($scope, $state, TreinamentosService) {
+.controller('porNoArCtrl', ['$scope', '$state', '$ionicPopup', 'TreinamentosService', 
+function ($scope, $state, $ionicPopup, TreinamentosService) {
     $scope.cria = function(treinamento) {
         treinamento.fim.setDate(treinamento.inicio.getDate());
         treinamento.fim.setMonth(treinamento.inicio.getMonth());
@@ -12,7 +12,11 @@ function ($scope, $state, TreinamentosService) {
                 $state.go('menu.fiqueLigado');
             },
             function(resposta) {
-                alert(resposta.data);
+                $ionicPopup.alert({
+                    title: 'Ops, algo deu errado...',
+                    okType: 'button-danger',
+                    template: resposta.data
+                });
             }
         );
     };
@@ -23,45 +27,57 @@ function ($scope) {
 
 }])
    
-.controller('fiqueLigadoCtrl', ['$scope', 'TreinamentosService', 
-function ($scope, TreinamentosService) {
+.controller('fiqueLigadoCtrl', ['$scope', '$ionicPopup', 'TreinamentosService', 
+function ($scope, $ionicPopup, TreinamentosService) {
     TreinamentosService.lista().then(
         function(resposta) {
             $scope.treinamentos = resposta.data;
         },
         function(resposta) {
-            alert(resposta.data);
+            $ionicPopup.alert({
+                title: 'Ops, algo deu errado...',
+                okType: 'button-danger',
+                template: resposta.data
+            });
         }
     );
 }])
    
-.controller('contagemRegressiva321Ctrl', ['$scope', 'TreinamentosService', 
-function ($scope, TreinamentosService) {
+.controller('contagemRegressiva321Ctrl', ['$scope', '$ionicPopup', 'TreinamentosService', 
+function ($scope, $ionicPopup, TreinamentosService) {
     TreinamentosService.lista().then(
         function(resposta) {
             $scope.treinamentos = resposta.data;
         },
         function(resposta) {
-            alert(resposta.data);
+            $ionicPopup.alert({
+                title: 'Ops, algo deu errado...',
+                okType: 'button-danger',
+                template: resposta.data
+            });
         }
     );
 }])
    
-.controller('vaiComecarCtrl', ['$scope', '$stateParams', 'TreinamentosService', 
-function ($scope, $stateParams, TreinamentosService) {
+.controller('vaiComecarCtrl', ['$scope', '$stateParams', '$ionicPopup', 'TreinamentosService', 
+function ($scope, $stateParams, $ionicPopup, TreinamentosService) {
     var id = $stateParams.id;
     TreinamentosService.buscaPorId(id).then(
         function(resposta) {
             $scope.treinamento = resposta.data;
         },
         function(resposta) {
-            alert(resposta.data);
+            $ionicPopup.alert({
+                title: 'Ops, algo deu errado...',
+                okType: 'button-danger',
+                template: resposta.data
+            });
         }
     );
 }])
    
-.controller('queroQueroCtrl', ['$scope', '$state', '$stateParams', 'TreinamentosService', 
-function ($scope, $state, $stateParams, TreinamentosService) {
+.controller('queroQueroCtrl', ['$scope', '$state', '$stateParams', '$ionicPopup', 'TreinamentosService', 
+function ($scope, $state, $stateParams, $ionicPopup, TreinamentosService) {
     $scope.inscreve = function(email) {
         var id = $stateParams.id;
             TreinamentosService.adiciona(id, email).then(
@@ -69,7 +85,11 @@ function ($scope, $state, $stateParams, TreinamentosService) {
                 $state.go('menu.aeeToDentro');
             },
             function(resposta) {
-                alert(resposta.data);
+                $ionicPopup.alert({
+                    title: 'Ops, algo deu errado...',
+                    okType: 'button-danger',
+                    template: resposta.data
+                });
             }
         );
     };
